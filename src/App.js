960f88cda48ce78448dd805c5343950d2f584ea3
jeffshen <jeffrey.shen@jpl.nasa.gov>
2018-01-17 12:54:01 -0800

    [refactor] refactored contents to dashboard component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Weather from './components/weather/weather';
+import Dashboard from './components/dashboard/dashboard';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Weather />
+      <div class="dashboard_div-container">
+        <Dashboard />
       </div>
     );
   }
