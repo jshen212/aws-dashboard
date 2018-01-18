6c1586d0206d9ec139ad53200c9f5553b72b8403
jeffshen <jeffrey.shen@jpl.nasa.gov>
2018-01-18 09:40:25 -0800

    [feature] added initial state

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,22 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './dashboard.css';
 import Weather from '../weather/weather';
 
 class Dashboard extends Component {
     // TODO: setup list of targets
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: {},
+            isLoading: true
+        };
+
+        navigator.geolocation.getCurrentPosition(position => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            const url = 'https://0hpr5r9oi2.execute-api.us-west-1.amazonaws.com/prod/getLatLonWeather';
+
+            // this.getWeatherData(url);
+        });
+
+    }
+
+    getWeatherData(url) {
+        axios.get(url).then(({ data }) => {
+            this.setState({ data });
+            console.log('initial state', this.state);
+        })
+        .catch(error => console.log(error));
+    }
+
+    isLoading(tOrF) {
+        this.setState({ isLoading: tOrF }) ;
+    }
     
     render() {
         return (
-            <div class="dashboard_view">
-                <div class="container_list-view">
-                    <div class="target_list-container">
-                        list of targets
-                        {/* TODO: iterate over list of targets */}
-                        <ul class="target_list-items">
-                            <li class="target_list-item"><Weather /></li>
+            <div className="dashboard_view">
+                <div className="container_list-view">
+                    <div className="target_list-container">
+                        <ul className="target_list-items">
+                            <li className="target_list-item"><Weather /></li>
                         </ul>
                     </div>
-                    <div class="target_detail-view">
+                    <div className="target_detail-view">
                         detail view of target
                     </div>
                 </div>
